44df93426e5806885164df04c30f63a27e5bff72
kudo <k.kudo125@gmail.com>
2020-11-20 19:48:28 +0900

    input完成11/20　19：48

---
+++ Assets/Kudo/Script/Input.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Input
+{
+    bool JumpCheck();
+    bool ColorCheck();
+    /// <summary>
+    /// bool値を　false　にする
+    /// </summary>
+    void Reset();
+
+}
