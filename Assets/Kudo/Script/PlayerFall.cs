ad8036f8df11746666cb8563e7d31e338d6faece
kudo <k.kudo125@gmail.com>
2020-11-25 14:21:39 +0900

    no message

--- Assets/Kudo/Script/PlayerFall.cs
+++ Assets/Kudo/Script/PlayerFall.cs
@@ -7,7 +7,7 @@ public class PlayerFall : MonoBehaviour
     /// <summary>
     /// プレイヤーのRigidbody
     /// </summary>
-    private Rigidbody playerRigidbody;
+    private Rigidbody m_playerRigidbody;
 
    
     /// <summary>
@@ -20,41 +20,37 @@ public class PlayerFall : MonoBehaviour
     /// </summary>
     [SerializeField] private float m_playerTransformLimit;
 
-    /// <summary>
-    /// fallのオンオフ切り替え用
-    /// </summary>
-    [SerializeField] private bool m_fallSwich=false;
 
-    [SerializeField] private float m_time;
 
 
 
     private void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
+        m_playerRigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            m_time = 0;
-            m_fallSwich = true;
-        }
-        if (Input.GetKeyUp(KeyCode.Space)) m_fallSwich = false;
-
-       
+        
     }
 
     private void FixedUpdate()
     {
-        playerRigidbody.AddForce(Vector3.down * gravity);
+        //stateがfallの時
+        //下方向に力を加える
+        m_playerRigidbody.AddForce(Vector3.down * gravity);
     }
 
 
     private void LateUpdate()
     {
-        if (transform.position.y < m_playerTransformLimit)transform.position =new Vector3(transform.position.x, m_playerTransformLimit,transform.position.z);
-        
+        if (m_playerRigidbody.velocity.y<0)
+        {
+            //m_playerTransformLimit=床のオブジェクト.y
+
+            if (transform.position.y < m_playerTransformLimit) transform.position = new Vector3(transform.position.x, m_playerTransformLimit, transform.position.z);
+
+        }
+
     }
 }
