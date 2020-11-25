9dc90c68e8af3f42cfe7da98e4f44d335a9d05ff
mogihayato <ibzbupnphj1a27@gmail.com>
2020-11-25 16:56:05 +0900

    no message

--- Assets/Mogi/Script/GameManager.cs
+++ Assets/Mogi/Script/GameManager.cs
@@ -13,43 +13,78 @@ public class GameManager : MonoBehaviour
     /// UIManagerが入っているゲームオブジェクト
     /// </summary>
     [SerializeField]
-    GameObject UIManagerObject;
+    GameObject m_UIManagerObject;
+
+    /// <summary>
+    /// Inputが入っているゲームオブジェクト
+    /// </summary>
+    [SerializeField]
+    GameObject m_InputObject;
 
     /// <summary>
     /// ゲームがスタートするとTRUEになる
     /// </summary>
-    private bool gamestarting = false;
-
+    private bool m_gamestarting = false;
+    /// <summary>
+    /// JumpCheckのリターン
+    /// </summary>
+    private bool m_jumpinput = false;
+    /// <summary>
+    /// ColorCheckのリターン
+    /// </summary>
+    private bool m_colorcheckinput = false;
     /// <summary>
     /// 時間
     /// </summary>]
     private float m_time=0;
+
+
+
+
 
     /// <summary>
     /// 時間を計ってTimerに時間の加算を頼む、送られた時間をUIManagerへ
     /// </summary>
     void Update()
     {
-        if (gamestarting)
+        if (m_gamestarting)
         {
-            m_time=tim.TimerCount(Time.deltaTime);
-            UIManagerObject.GetComponent<UIManager>().TimerOutput(m_time);
+            //Input呼び出し
+            m_jumpinput= m_InputObject.GetComponent<SmartPhoneInput>().JumpCheck();
+
+            //Input呼び出し
+            m_colorcheckinput = m_InputObject.GetComponent<SmartPhoneInput>().ColorCheck();
+
+            //m_InputObject.GetComponent<SmartPhoneInput>().LeftScreenTap();
+            //タイマーカウント呼び出し
+            m_time =tim.TimerCount(Time.deltaTime);
+
+            //UIManagerでタイマー表示
+            m_UIManagerObject.GetComponent<UIManager>().TimerOutput(m_time);
         }
     }
+
+
+
+
     /// <summary>
     /// ゲームのスタート
     /// </summary>
     public void GameStart()
     {
-        gamestarting = true;
+        m_gamestarting = true;
         tim.TimerStart();
     }
+
+
+
+
     /// <summary>
     /// ゲームの終わり
     /// </summary>
     public void GameEnd()
     {
-        gamestarting = false;
+        m_gamestarting = false;
     }
 
 }
