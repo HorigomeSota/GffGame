5526b7371b1697121521b1e494297ebae83fc0b0
DESKTOP-RSVPM06\ytenp <ytenpesto@gmail,com>
2020-11-26 12:20:34 +0900

    trigger連続で同じオブジェクト触れても大丈夫なように+優先度アルゴリズム修正

--- Assets/Hemmi/Sc/Triggers.cs
+++ Assets/Hemmi/Sc/Triggers.cs
@@ -15,7 +15,7 @@ public class Triggers : MonoBehaviour
     private int m_priorityExit;//今出ていったオブジェクトの優先度
 
     [SerializeField] private GameObject m_player;//プレイヤー取得
-
+    bool m_nullFrag = false;
 
     private void Start()
     {
@@ -35,6 +35,22 @@ public class Triggers : MonoBehaviour
     private void FixedUpdate()
     {
         GetComponent<PlayerState>().SetTriggerObj(g_mostPriority);//プレイヤーのゲームオブジェクト受け取るやつ呼び出すm_player.GetComponent<プレイヤーステイト>().呼び出すやつ(g_mostPriority);
+
+        
+        int m_nullLoop = 4;
+        while (m_nullLoop >= 0)
+        {
+            if (m_triggerStays[m_priorityExit] == null)
+            {
+
+            }
+            else
+            {
+                m_nullFrag = true;
+                m_nullLoop = -1;
+            }
+            m_nullLoop--;
+        }
 
     }
 
@@ -100,24 +116,55 @@ public class Triggers : MonoBehaviour
                 m_priorityExit = 4;
                 break;
         }
-        m_triggerStays[m_priorityExit] = null;
-        m_prioritys[m_priorityExit] = -1;
-
-        if (m_priorityMax== m_priorityExit)//もし、離れたオブジェクトの優先度が、最高優先度だったら、次に優先度の高いオブジェクトを最高優先度に設定する
+        if(other.gameObject== m_triggerStays[m_priorityExit])//離れたオブジェクトと触れていたオブジェクトが一致していたら
         {
-            int m_rePriorityMax = 4;
-            while (m_rePriorityMax>=0)
+            Debug.Log("はなれたあああああ");
+            m_triggerStays[m_priorityExit] = null;
+            m_prioritys[m_priorityExit] = -1;
+            //もし、なんのオブジェクトにも触れていなかったら、優先度を最低にするためのフラグON
+            //bool m_nullFrag = false;
+            int m_nullLoop = 4;
+            while (m_nullLoop >= 0)
             {
-
-                if(Array.IndexOf(m_prioritys, m_rePriorityMax) != -1)
+                if (m_triggerStays[m_priorityExit] == null)
                 {
-                    m_priorityMax = m_prioritys[Array.IndexOf(m_prioritys, m_rePriorityMax)];
+
                 }
-                g_mostPriority = m_triggerStays[m_priorityMax];
-                m_rePriorityMax--;
+                else { 
+                    m_nullFrag = true;
+                    m_nullLoop = -1;
+                }
+                m_nullLoop--;
             }
-            
+
+
+            //優先度再設定
+            if (m_nullFrag)//触れているオブジェクトなし
+            {
+                m_priorityMax = 5;
+                g_mostPriority = null;
+            }
+            else
+            if (m_priorityMax == m_priorityExit)//もし、離れたオブジェクトの優先度が、最高優先度だったら、次に優先度の高いオブジェクトを最高優先度に設定する
+            {
+                int m_rePriorityMax = 4;
+                while (m_rePriorityMax >= 0)
+                {
+
+                    if (Array.IndexOf(m_prioritys, m_rePriorityMax) != -1)
+                    {
+                        m_priorityMax = m_prioritys[Array.IndexOf(m_prioritys, m_rePriorityMax)];
+                    }
+                    g_mostPriority = m_triggerStays[m_priorityMax];
+                    m_rePriorityMax--;
+                }
+
+            }
         }
+
+        
+
+        
         
 
     }
