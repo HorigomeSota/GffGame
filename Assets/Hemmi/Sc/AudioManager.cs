5157abc59ebe054a8c05453ecd5341ae3b6ca53e
DESKTOP-RSVPM06\ytenp <ytenpesto@gmail,com>
2020-11-30 19:49:16 +0900

    オーディオマネージャー作成中1

---
+++ Assets/Hemmi/Sc/AudioManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_audioBGMObject;
+    [SerializeField]
+    private GameObject m_audioSEObject;
+
+    [SerializeField]
+    private AudioSource m_audioSourceBGM;
+    [SerializeField]
+    private AudioSource m_audioSourceSE;
+
+
+    [SerializeField]
+    private AudioClip m_jump;
+    [SerializeField]
+    private AudioClip m_stage;
+    [SerializeField]
+    private AudioClip m_colorChange;
+    [SerializeField]
+    private AudioClip m_boost;
+    [SerializeField]
+    private AudioClip m_panel;
+
+
+
+    // 音声再生用スクリプト
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_audioSourceBGM = m_audioBGMObject.GetComponent<AudioSource>();
+        m_audioSourceSE = m_audioSEObject.GetComponent<AudioSource>();
+    }
+
+    private void PlayBGM()
+    {
+        m_audioSourceBGM.Play();
+    }
+
+    private void PlaySE()
+    {
+        m_audioSourceSE.PlayOneShot(m_audioSourceSE.clip);
+    }
+
+    public void AudioStopBGM()//BGM止める
+    {
+        m_audioSourceBGM.Stop();
+    }
+
+    public void AudioStopSE()//SE止める
+    {
+        m_audioSourceSE.Stop();
+    }
+
+    public void StageBGM()//StageBGM再生
+    {
+        Debug.Log("BGM");
+        m_audioSourceBGM.clip = m_stage;
+        PlayBGM();
+    }
+
+    public void JumpSE()//JumpSE再生
+    {
+        m_audioSourceSE.clip = m_jump;
+        PlaySE();
+    }
+
+    public void ColorChangeSE()//ColorChangeSE再生
+    {
+        m_audioSourceSE.clip = m_colorChange;
+        PlaySE();
+    }
+
+    public void BoostSE()//BoostSE再生
+    {
+        m_audioSourceSE.clip = m_boost;
+        PlaySE();
+    }
+
+    public void PanelSE()//PanelSE再生
+    {
+        m_audioSourceSE.clip = m_panel;
+        PlaySE();
+    }
+}
--- Assets/Mogi/Script/GameManager.cs
+++ Assets/Mogi/Script/GameManager.cs
@@ -7,7 +7,27 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Timer格納用の変数。インスタンス化済み
     /// </summary>
-    Timer tim = new Timer();
+    Timer m_tim = new Timer();
+
+    /// <summary>
+    /// AudioManager格納用変数
+    /// </summary>
+    AudioManager m_audioManager;
+
+    /// <summary>
+    /// PlayerState格納用変数
+    /// </summary>
+    PlayerState m_playerState;
+
+    /// <summary>
+    /// Input格納用変数
+    /// </summary>
+    IInput m_input=new SmartPhoneInput();
+
+    /// <summary>
+    /// UIManager格納用変数
+    /// </summary>
+    UIManager m_UIManager;
 
     /// <summary>
     /// UIManagerが入っているゲームオブジェクト
@@ -16,16 +36,26 @@ public class GameManager : MonoBehaviour
     GameObject m_UIManagerObject;
 
     /// <summary>
-    /// Inputが入っているゲームオブジェクト
+    /// AudioManagerが入っているゲームオブジェクト
     /// </summary>
     [SerializeField]
-    GameObject m_InputObject;
+    GameObject m_audioManagerObject;
+
+    /*
+    /// <summary>
+    /// timerが入ってるオブジェクト
+    /// </summary>
+    [SerializeField]
+    GameObject m_timerObject;
+    */
 
     /// <summary>
     /// PlayerStateが入っているゲームオブジェクト
     /// </summary>
     [SerializeField]
-    GameObject m_PlayreState;
+    GameObject m_playerStateObject;
+
+
 
     /// <summary>
     /// ゲームがスタートするとTRUEになる
@@ -39,14 +69,34 @@ public class GameManager : MonoBehaviour
     /// ColorCheckのリターン
     /// </summary>
     private bool m_colorcheckinput = false;
+
+    /// <summary>
+    /// オーディオが再生中かどうか
+    /// </summary>
+    private bool m_playAudio;
+
     /// <summary>
     /// 時間
-    /// </summary>]
+    /// </summary>
     private float m_time=0;
 
+    private void Awake()
+    {
+        //インスタンス化
+        m_audioManager = m_audioManagerObject.GetComponent<AudioManager>();
+        m_playerState = m_playerStateObject.GetComponent<PlayerState>();
+        m_UIManager = m_UIManagerObject.GetComponent<UIManager>();
+
+        //if(スマートフォンなら)
+        m_input = new SmartPhoneInput();
 
 
+    }
 
+    private void Start()
+    {
+        GameStart();
+    }
 
     /// <summary>
     /// 時間を計ってTimerに時間の加算を頼む、送られた時間をUIManagerへ
@@ -58,52 +108,76 @@ public class GameManager : MonoBehaviour
 
 
             //Inputのジャンプ呼び出し
-            m_jumpinput= m_InputObject.GetComponent<IInput>().JumpCheck();
+            m_jumpinput= m_input.JumpCheck();
 
             //Inputのカラーチェンジ呼び出し
-            m_colorcheckinput = m_InputObject.GetComponent<IInput>().ColorCheck();
+            m_colorcheckinput = m_input.ColorCheck();
+
+
+            if (m_playerState.GetJumpFlag())
+            {
+                m_audioManager.JumpSE();
+            }
+            else if (m_playerState.GetBoostFlag())
+            {
+                m_audioManager.BoostSE();
+            }
+            else if (m_playerState.GetColorChangeFlag())
+            {
+                m_audioManager.ColorChangeSE();
+            }
+            else if (m_playerState.GetPanelSpeedUpFlag())
+            {
+                m_audioManager.PanelSE();
+            }
+            else if (m_playerState.GetDeathFlag())
+            {
+
+            }
+            else
+            {
+                //m_playAudio = false;
+            }
+
+
 
             //ジャンプとカラーチェンジの条件判定
             if (m_jumpinput)
             {
-                m_PlayreState.GetComponent<PlayerState>().JumpFlagOn();
-                Debug.Log("ジャンプ");
+                m_playerState.JumpFlagOn();
+                //Debug.Log("ジャンプ");
                 m_jumpinput = false;
+                
             }
             if (m_colorcheckinput)
             {
-
-                m_PlayreState.GetComponent<PlayerState>().ColorChangeFlagOn();
+                m_playerState.ColorChangeFlagOn();
                 m_colorcheckinput = false;
             }
             //リセット
-            m_InputObject.GetComponent<IInput>().Reset();
+            m_input.Reset();
 
 
             //タイマーカウント呼び出し
-            m_time =tim.TimerCount(Time.deltaTime);
+            m_time = m_tim.TimerCount(Time.deltaTime);
 
             //UIManagerでタイマー表示
-            m_UIManagerObject.GetComponent<UIManager>().TimerOutput(m_time);
+            m_UIManager.TimerOutput(m_time);
         }
 
-        if (m_PlayreState.GetComponent<PlayerState>().GetDeathFlag() == true) GameEnd();
+        if (m_playerState.GetDeathFlag() == true) GameEnd();
     }
-
-
-
 
     /// <summary>
     /// ゲームのスタート
     /// </summary>
-    public void GameStart()
+    private void GameStart()
     {
         m_gamestarting = true;
-        tim.TimerStart();
+        m_audioManager.StageBGM();
+        m_tim.TimerStart();
+
     }
-
-
-
 
     /// <summary>
     /// ゲームの終わり
