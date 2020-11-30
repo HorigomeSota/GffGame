19f8fdba7ad592ecacb3d3a5dbf22c76824a9aa9
HorigomeSota <s.horigome2000@gmail.com>
2020-11-30 19:49:53 +0900

    stage外部ファイルから読み込み、とりあえず最初

---
+++ Assets/Horigome/Script/CSVread.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class CSVread : MonoBehaviour
+{
+    private int[,] stageMapDatas;
+    public int[,] GetStageMapDatas()
+    {
+        return stageMapDatas;
+    }
+
+    // CSVから切り分けられた文字列型２次元配列データ 
+    private string[,] sdataArrays;
+
+    //読み込めたか確認の表示用の変数
+    private int height = 0;    //行数
+    public int GetHeight()
+    {
+        return height;
+    }
+    private int width = 0;    //列数
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    void Start()
+    {
+        //データパスを設定
+        //このデータパスは、Assetフォルダ以下の位置を書くので/で階層を区切り、CSVデータ名まで書かないと読み込んでくれない
+        //string path = "/StreamingAssets/Stage.csv";
+        //データを読み込む(引数：データパス)
+
+        //readCSVData(Application.dataPath + path, ref this.sdataArrays);
+        //convert2DArrayType(ref this.sdataArrays, ref this.stageMapDatas, this.height, this.width);
+
+        //WriteMapDatas(this.stageMapDatas, this.height, this.width);
+    }
+
+    // CSVデータを文字列型２次元配列に変換する
+    //                      ファイルパス,変換される配列の値(参照渡し)
+    private void readCSVData(string path, ref string[,] sdata)
+    {
+        // ストリームリーダーsrに読み込む
+        StreamReader sr = new StreamReader(path);
+        // ストリームリーダーをstringに変換
+        string strStream = sr.ReadToEnd();
+        // StringSplitOptionを設定(要はカンマとカンマに何もなかったら格納しないことにする)
+        StringSplitOptions option = StringSplitOptions.RemoveEmptyEntries;
+
+        // 行に分ける
+        string[] lines = strStream.Split(new char[] { '\r', '\n' }, option);
+
+        // カンマ分けの準備(区分けする文字を設定する)
+        char[] spliter = new char[1] { ',' };
+
+        // 行数設定
+        int h = lines.Length;
+        // 列数設定
+        int w = lines[0].Split(spliter, option).Length;
+
+        // 返り値の2次元配列の要素数を設定
+        sdata = new string[h, w];
+
+        // 行データを切り分けて,2次元配列へ変換する
+        for (int i = 0; i < h; i++)
+        {
+            string[] splitedData = lines[i].Split(spliter, option);
+
+            for (int j = 0; j < w; j++)
+            {
+                sdata[i, j] = splitedData[j];
+            }
+        }
+
+        // 確認表示用の変数(行数、列数)を格納する
+        this.height = h;    //行数   
+        this.width = w;    //列数
+    }
+
+    // ２次元配列の型を文字列型から整数値型へ変換する
+    private void convert2DArrayType(ref string[,] sarrays, ref int[,] iarrays, int h, int w)
+    {
+        iarrays = new int[h, w];
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                iarrays[i, j] = int.Parse(sarrays[i, j]);
+            }
+        }
+    }
+
+    /// <summary>
+    /// CSV読み込んで準備
+    /// </summary>
+    public void PrepareStage(string path)
+    {
+        path = "/StreamingAssets/" + path + ".csv";
+        readCSVData(Application.dataPath + path, ref this.sdataArrays);
+        convert2DArrayType(ref this.sdataArrays, ref this.stageMapDatas, this.height, this.width);
+    }
+
+
+    //確認表示用の関数
+    //引数：2次元配列データ,行数,列数
+    private void WriteMapDatas(int[,] arrays, int hgt, int wid)
+    {
+        for (int i = 0; i < hgt; i++)
+        {
+
+            for (int j = 0; j < wid; j++)
+            {
+                //行番号-列番号:データ値 と表示される
+                Debug.Log(i + "-" + j + ":" + arrays[i, j]);
+            }
+        }
+    }
+}
---
+++ Assets/Horigome/Script/StageCreate.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject floorAObj;
+    const int FLOOR_A = 10;
+    [SerializeField]
+    GameObject floorBObj;
+    const int FLOOR_B = 11;
+    [SerializeField]
+    GameObject toleranceAObj;
+    const int TOLERANCE_A = 20;
+    [SerializeField]
+    GameObject toleranceBObj;
+    const int TOLERANCE_B = 21;
+    [SerializeField]
+    GameObject enemyAObj;
+    const int ENEMY_A = 30;
+    [SerializeField]
+    GameObject enemyBObj;
+    const int ENEMY_B = 31;
+
+    [SerializeField]
+    const int FLOORA_ENEMYA = 1030;
+    [SerializeField]
+    const int FLOORA_ENEMYB = 1031;
+    [SerializeField]
+    const int FLOORB_ENEMYA = 1130;
+    [SerializeField]
+    const int FLOORB_ENEMYB = 1131;
+
+    #region GameObject panelAObj
+    [SerializeField]
+    GameObject panelA0Obj;
+    const int PANEL_A_0 = 4000;
+    [SerializeField]
+    GameObject panelA15Obj;
+    const int PANEL_A_15 = 4015;
+    [SerializeField]
+    GameObject panelA30Obj;
+    const int PANEL_A_30 = 4030;
+    [SerializeField]
+    GameObject panelA45Obj;
+    const int PANEL_A_45 = 4045;
+    [SerializeField]
+    GameObject panelA60Obj;
+    const int PANEL_A_60 = 4060;
+    [SerializeField]
+    GameObject panelA75Obj;
+    const int PANEL_A_75 = 4075;
+    [SerializeField]
+    GameObject panelA90Obj;
+    const int PANEL_A_90 = 4090;
+    #endregion
+    #region GameObject panelBObj
+    [SerializeField]
+    GameObject panelB0Obj;
+    const int PANEL_B_0 = 4100;
+    [SerializeField]
+    GameObject panelB15Obj;
+    const int PANEL_B_15 = 4115;
+    [SerializeField]
+    GameObject panelB30Obj;
+    const int PANEL_B_30 = 4130;
+    [SerializeField]
+    GameObject panelB45Obj;
+    const int PANEL_B_45 = 4145;
+    [SerializeField]
+    GameObject panelB60Obj;
+    const int PANEL_B_60 = 4160;
+    [SerializeField]
+    GameObject panelB75Obj;
+    const int PANEL_B_75 = 4175;
+    [SerializeField]
+    GameObject panelB90Obj;
+    const int PANEL_B_90 = 4190;
+    #endregion
+    [SerializeField]
+    GameObject shortcutObj;
+    const int SHORTCUT = 50;
+
+    [SerializeField]
+    GameObject checkPointObj;
+    const int CHECKPOINT = 100;
+
+    GameObject checkPointObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CSVread>().PrepareStage("Stage01");
+        CreateMap(GetComponent<CSVread>().GetStageMapDatas(), GetComponent<CSVread>().GetHeight(), GetComponent<CSVread>().GetWidth());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateMap(int[,] arrays, int hgt, int wid)
+    {
+        for (int i = 0; i < hgt; i++)
+        {
+            for (int j = 0; j < wid; j++)
+            {
+                switch (arrays[i, j])
+                {
+                    case FLOOR_A:
+                        Instantiate(floorAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case FLOOR_B:
+                        Instantiate(floorBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case TOLERANCE_A:
+                        Instantiate(toleranceAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case TOLERANCE_B:
+                        Instantiate(toleranceBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case ENEMY_A:
+                        Instantiate(enemyAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case ENEMY_B:
+                        Instantiate(enemyBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case SHORTCUT:
+                        Instantiate(shortcutObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+
+                    #region case PANEL_A
+                    case PANEL_A_0:
+                        Instantiate(panelA0Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_15:
+                        Instantiate(panelA15Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_30:
+                        Instantiate(panelA30Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_45:
+                        Instantiate(panelA45Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_60:
+                        Instantiate(panelA60Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_75:
+                        Instantiate(panelA75Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_A_90:
+                        Instantiate(panelA90Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    #endregion
+                    #region case PANEL_B
+                    case PANEL_B_0:
+                        Instantiate(panelB0Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_15:
+                        Instantiate(panelB15Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_30:
+                        Instantiate(panelB30Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_45:
+                        Instantiate(panelB45Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_60:
+                        Instantiate(panelB60Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_75:
+                        Instantiate(panelB75Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case PANEL_B_90:
+                        Instantiate(panelB90Obj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    #endregion
+
+                    case CHECKPOINT:
+                        checkPointObject = Instantiate(checkPointObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+
+                    case FLOORA_ENEMYA:
+                        Instantiate(floorAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        Instantiate(enemyAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case FLOORA_ENEMYB:
+                        Instantiate(floorAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        Instantiate(enemyBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case FLOORB_ENEMYA:
+                        Instantiate(floorBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        Instantiate(enemyAObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                    case FLOORB_ENEMYB:
+                        Instantiate(floorBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        Instantiate(enemyBObj, new Vector3(transform.position.x + j, transform.position.y + hgt - 1 - i, transform.position.z), Quaternion.identity);
+                        break;
+                }
+            }
+        }
+        transform.position = checkPointObject.transform.position;
+    }
+}
