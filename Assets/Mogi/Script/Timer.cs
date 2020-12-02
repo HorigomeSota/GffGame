1d8dc6316e51adf09236004c81afbc397ace3f09
mogihayato <ibzbupnphj1a27@gmail.com>
2020-12-02 12:27:44 +0900

    TimerとUiManagerをTimeDataと連携して動かすように対応

--- Assets/Mogi/Script/Timer.cs
+++ Assets/Mogi/Script/Timer.cs
@@ -5,33 +5,44 @@ using UnityEngine.UI;
 
 class Timer : MonoBehaviour
 {
+    TimeData m_timedata;
+    /// <summary>
+    /// TimeDataが入っているオブジェクトのタグ名
+    /// </summary>
+    const string m_datatag = "Data";
     /// <summary>
     /// 数字を表示するテキスト
     /// </summary>
     private Text timerText;
+
+    /// <summary>
+    /// プレイ中か判断するためのもの
+    /// </summary>
+    bool m_playnow;
 
 
     /// <summary>
     /// 秒数
     /// </summary>
     private float m_time;
-
-
+    private void Start()
+    {
+        m_timedata = GameObject.FindGameObjectWithTag(m_datatag).GetComponent<TimeData>();
+    }
     /// <summary>
     /// タイマーの初期化
     /// </summary>
-    public void TimerStart()
+    public void TimerReset()
     {
         m_time = 0;
     }
 
-
     /// <summary>
     /// GameManagerから呼び出される、秒数を加算して返す
     /// </summary>
-    public float TimerCount(float delta)
+    public void TimerCount(float delta)
     {
         m_time += delta;
-        return m_time;
+        m_timedata.PlayNowTimeSave(m_time);
     }
 }
--- Assets/Mogi/Script/UIManager.cs
+++ Assets/Mogi/Script/UIManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    TimeData m_timedata;
+    /// <summary>
+    /// TimeDataが入っているオブジェクトのタグ名
+    /// </summary>
+    const string m_datatag = "Data";
+
     //　タイマー表示用テキスト
     [SerializeField]
     GameObject timerText;
@@ -17,16 +23,24 @@ public class UIManager : MonoBehaviour
     /// </summary>
     private int comma;
 
+
+    private void Start()
+    {
+        m_timedata = GameObject.FindGameObjectWithTag(m_datatag).GetComponent<TimeData>();
+    }
+
+
     /// <summary>
     /// GameManagerから呼び出されて時間を加算して表示する。※要ゲームオブジェクトへのアタッチ
     /// </summary>
-    /// <param name="time"></param>
-    public void TimerOutput(float time)
+    /// <param name="g_time"></param>
+    public void TimerOutput()
     {
-        if (time != 0)
+        float g_time = m_timedata.GetPlayNow();
+        if (g_time != 0)
         {
-            seconds = (int)(time - time % 1);
-            comma = (int)(time % 1*100);
+            seconds = (int)(g_time - g_time % 1);
+            comma = (int)(g_time % 1*100);
         }
 
         timerText.GetComponent<Text>().text = seconds + ":" + comma;
